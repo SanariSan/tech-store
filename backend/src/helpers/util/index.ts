dc8305ac0720f78db8a54de1a1f7276077a572c3
SanariSan <SanariSan@users.noreply.github.com>
2023-04-16 05:57:28 +0400

    fix: another one

--- backend/src/helpers/util/index.ts
+++ backend/src/helpers/util/index.ts
@@ -2,7 +2,6 @@ export * from './clear-cli.util';
 export * from './duplicate-n-times.util';
 export * from './int-in-range.util';
 export * from './map';
-export * from './algorithms';
 export * from './random-hex.util';
 export * from './sleep.util';
 export * from './strict-checkers';
