1eaee91e3c8ba9808d86cc5f04f422aea9160cbc
SanariSan <SanariSan@users.noreply.github.com>
2023-05-10 20:09:08 +0400

    feat: catalogue api change

--- backend/src/server/controllers/goods/categories/categories.controller.ts
+++ backend/src/server/controllers/goods/categories/categories.controller.ts
@@ -3,17 +3,60 @@ import type { TRequestNarrowed } from '../../../express.type';
 import { SuccessResponse } from '../../../responses';
 
 export const categoriesCTR = (req: TRequestNarrowed, res: Response, next: NextFunction) => {
-  let x;
+  const categories = [
+    {
+      title: 'laptops',
+      sub: [
+        {
+          title: 'gaming',
+        },
+        {
+          title: 'work',
+        },
+        {
+          title: 'chill',
+        },
+      ],
+    },
+    {
+      title: 'phones',
+      sub: [
+        {
+          title: 'hi-tech',
+        },
+        {
+          title: 'goofy',
+        },
+        {
+          title: 'boomer',
+        },
+      ],
+    },
+    {
+      title: 'accessories',
+      sub: [
+        {
+          title: 'charger',
+        },
+        {
+          title: 'headphones',
+        },
+      ],
+    },
+  ];
 
   return new SuccessResponse({
     res,
     data: {
-      categories: ['laptops', 'phones', 'accessories'],
-      subCategories: {
-        laptops: ['gaming', 'work', 'chill'],
-        phones: ['hi-tech', 'goofy', 'boomer'],
-        accessories: ['charger', 'headphones'],
-      },
+      categories,
     },
+    // {
+    //   categories: ['laptops', 'phones', 'accessories'],
+    //   subCategories: {
+    //     laptops: ['gaming', 'work', 'chill'],
+    //     phones: ['hi-tech', 'goofy', 'boomer'],
+    //     accessories: ['charger', 'headphones'],
+    //   },
+    // },
   }).send();
 };
