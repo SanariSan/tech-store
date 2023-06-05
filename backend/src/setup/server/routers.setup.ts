cdff204e1b591e49d8381dd24cc88e073fa853f9
SanariSan <SanariSan@users.noreply.github.com>
2023-06-06 02:27:07 +0400

    feat: figuring out, no nginx now

--- backend/src/setup/server/routers.setup.ts
+++ backend/src/setup/server/routers.setup.ts
@@ -1,27 +1,29 @@
+import express from 'express';
 import type { Express } from 'express';
+import path from 'path';
 import { NotFoundError } from '../../server/error';
 import type { TApiBranches } from '../../server/routers';
 import * as apiBranches from '../../server/routers';
 
 function setupRoutersExpress(app: Express) {
-  const { API_VERSION } = process.env;
+  const { API_VERSION, NODE_ENV, BUILD_PATH } = process.env;
 
   // console.dir(apiBranches, { depth: 10 }); // => { v1: [Getter], v2: [Getter] }
   app.use(`/api/${API_VERSION}`, (apiBranches as unknown as TApiBranches)[API_VERSION]);
+
+  // serve static content | moved to nginx
+  if (NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(BUILD_PATH)));
+    app.get('*', (req, res) => {
+      console.log('static');
+      res.sendFile(path.resolve(BUILD_PATH, 'index.html'));
+    });
+  }
 
   // general fallback when no static content is served
   app.use(`*`, () => {
     throw new NotFoundError({ message: 'Route not found' });
   });
-
-  // serve static content | moved to nginx
-  // if (NODE_ENV === 'production') {
-  //   app.use(express.static(path.resolve(BUILD_PATH)));
-  //   app.get('*', (req, res) => {
-  //     console.log('this');
-  //     res.sendFile(path.resolve(BUILD_PATH, 'index.html'));
-  //   });
-  // }
 }
 
 export { setupRoutersExpress };
